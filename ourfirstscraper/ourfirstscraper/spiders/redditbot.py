80dce7f508a3c12b5cdb606d02a26584743e610a
Yitzchak Meirovich <ymeirovich@gmail.com>
2019-06-25 15:25:34 +0300

    third commit

--- ourfirstscraper/ourfirstscraper/spiders/redditbot.py
+++ ourfirstscraper/ourfirstscraper/spiders/redditbot.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
-import scrapy
-import urllib, mimetypes
-import traceback
+import mimetypes
 import ssl
+import traceback
+import urllib
 
+import scrapy
 from scrapy.spidermiddlewares.httperror import HttpError
 from twisted.internet.error import DNSLookupError, TCPTimedOutError
-
+from scrapy.linkextractors import LinkExtractor
+from urllib.parse import urlparse
 
 class RedditbotSpider(scrapy.Spider):
     name = 'redditbot'
@@ -27,35 +29,42 @@ class RedditbotSpider(scrapy.Spider):
         }
         for u in self.start_urls:
             yield scrapy.Request(u, callback=self.parse,
-                                    errback=self.errback_httpbin)
+                                    errback=self.errback_httpbin,dont_filter = True)
 
 
     def parse(self, response):
-        self.state['items_count'] = self.state.get('items_count', 0) + 1
-        # Extracting the content using css selectors
-        titles = response.css('a::attr(href)').getall()
-        # votes = response.css('').extract()
-        # times = response.css('time::attr(title)').extract()
-        # comments = response.css('.comments::text').extract()
+        try:
+            # self.links.clear()
+            self.state['items_count'] = self.state.get('items_count', 0) + 1
+            # Extracting the content using css selectors
+            # titles = response.css('a::attr(href)').getall()
+            # extract links from pages
+            links = LinkExtractor.extract_links(response)
+            # Give the extracted content row wise
+            for item in zip(links):
+                # votes, times, comments
+                # create a dictionary to store the scraped info
+                if item[0].find('http') == 0 and self.check_mime_type(item[0]):
+                    if item not in self.start_urls:
+                        self.links.append(item[0])
+                        scraped_info = {
+                            'url': item[0],
+                            'depth': response.meta['depth'],
+                            'ratio': 0,
+                            'domain': urlparse(item[0]).netloc
+                        }
+                # yield or give the scraped info to scrapy
+                    yield scraped_info
 
-        # Give the extracted content row wise
-        for item in zip(titles):
-            # votes, times, comments
-            # create a dictionary to store the scraped info
-            if item[0].find('http') == 0 and self.check_mime_type(item[0]):
-                self.links.append(item[0])
-                scraped_info = {
-                    'title': item[0],
-                    # 'vote': item[1],
-                    # 'created_at': item[2],
-                    # 'comments': item[3],
-                }
-            # yield or give the scraped info to scrapy
-                yield scraped_info
-        next_page = self.links.pop(0)
-        if next_page is not None:
-            yield scrapy.Request(next_page, callback=self.parse, errback=self.errback_httpbin)
-            # yield response.follow(next_page, callback=self.parse)
+            self.removeLinkDuplicates()
+            next_page = self.links.pop(0)
+            if next_page is not None:
+                yield scrapy.Request(next_page, callback=self.parse, errback=self.errback_httpbin,dont_filter = True)
+                # yield response.follow(next_page, callback=self.parse)
+        except Exception as e:
+            print("type error: " + str(e))
+            print(traceback.format_exc())
+
 
     def check_mime_type(self, url):
         try:
@@ -82,7 +91,7 @@ class RedditbotSpider(scrapy.Spider):
         print(repr(failure))
         next_page = self.links.pop(0)
         if next_page is not None:
-            yield scrapy.Request(next_page, callback=self.parse, errback=self.errback_httpbin)
+            yield scrapy.Request(next_page, callback=self.parse, errback=self.errback_httpbin,dont_filter = True)
         # in case you want to do something special for some errors,
         # you may need the failure's type:
 
@@ -91,18 +100,39 @@ class RedditbotSpider(scrapy.Spider):
             # you can get the non-200 response
             response = failure.value.response
             self.logger.info('HttpError on %s', response.url)
-            yield scrapy.Request(next_page, callback=self.parse, errback=self.errback_httpbin)
+            yield scrapy.Request(next_page, callback=self.parse, errback=self.errback_httpbin,dont_filter = True)
 
         elif failure.check(DNSLookupError):
             # this is the original request
             request = failure.request
             self.logger.info('DNSLookupError on %s', request.url)
-            yield scrapy.Request(next_page, callback=self.parse, errback=self.errback_httpbin)
+            yield scrapy.Request(next_page, callback=self.parse, errback=self.errback_httpbin,dont_filter = True)
 
         elif failure.check(TimeoutError, TCPTimedOutError):
             request = failure.request
             self.logger.info('TimeoutError on %s', request.url)
-            yield scrapy.Request(next_page, callback=self.parse, errback=self.errback_httpbin)
+            yield scrapy.Request(next_page, callback=self.parse, errback=self.errback_httpbin,dont_filter = True)
+
+    def removeLinkDuplicates(self):
+        try:
+            self.links = list(dict.fromkeys(self.links))  # remove list duplicates, creating new dict removes duplicates
+        except Exception as e:
+            print("type error: " + str(e))
+            print(traceback.format_exc())
+
+class LinkStore(scrapy.Item):
+
+    _store=[]
+
+    def getStore(self):
+        return self._store
+
+    def createStore(self):
+        pass
+
+
+
+
 
 """
 Requirements:
@@ -113,7 +143,7 @@ Requirements:
 3. √ extract fixed <a>, not dynamic inserted links (how do you tell the difference)
 4. per page calculate the ratio of same-domain:diff-domain links
     - same domain links have the same common prefix including subdomain
-5. crawler should cache last work done to restart where left off after termination
+5. √ crawler should cache last work done to restart where left off after termination
 6. schedule crawler
     - prevent duplicate downloads
     - check when page was last updated --> need to store lastUpdateDate field for each page
